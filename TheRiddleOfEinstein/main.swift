df59f0d8d6673dba44e9cfce5c1c3aa620cb98fb
tik-tak-ololo <43583365+tik-tak-ololo@users.noreply.github.com>
2020-09-02 22:30:05 +0700

    Initial Commit

---
+++ TheRiddleOfEinstein/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TheRiddleOfEinstein
+//
+//  Created by Sergey Khmelow on 02.09.2020.
+//  Copyright © 2020 Sergey Khmelow. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
